20034c588867d3c6c32743ab956a2d16c284967e
sookyoung <topgdvidsyb@naver.com>
2024-03-01 13:37:35 +0900

    chore: 초기 세팅

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import fs from 'fs';
 
 import routes from './routes';
 import db from './models';
@@ -7,9 +9,17 @@ import db from './models';
 const app: Express = express();
 dotenv.config();
 
+app.use(cors({ origin: true, credentials: true }));
+app.use('/upload', express.static('upload'));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '1mb', extended: false }));
+
 const connectToDB = async () => {
   try {
     await db.sequelize.sync({ force: false });
+    if (!fs.existsSync('./upload/'))
+      fs.mkdirSync('./upload/', { recursive: true });
   } catch (error) {
     console.log(error);
   }
@@ -18,6 +28,9 @@ const connectToDB = async () => {
 connectToDB();
 
 app.use('/api', routes);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.listen(8081, () => {
   console.log('Server Opened');
